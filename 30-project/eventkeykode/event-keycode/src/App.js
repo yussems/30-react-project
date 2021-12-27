898d254832b70b16faca18565bd3d1f46c59eff5
yussems <yussems@gmail.com>
2021-12-27 20:33:14 +0300

    finished keycode project

--- 30-project/eventkeykode/event-keycode/src/App.js
+++ 30-project/eventkeykode/event-keycode/src/App.js
@@ -1,16 +1,33 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [key, setKey] = useState("");
+  const [code, setCode] = useState("");
+  useEffect(() => {
+    window.addEventListener("keydown", (e) => {
+      setKey(e.key);
+      setCode(e.code);
+    });
+  }, [key]);
+
+  if (key === '') {
+    return (
+      <div className="first">
+        <small> Press any key to get the JavaScript event keycode</small>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <div className='key'>
+      <div className="key">
+      {key}
         <small>event.key</small>
       </div>
-      <div className='key'>
+      <div className="key">
+      {code}
         <small>event.keycode</small>
-      </div>
-      <div className='key'>
-        <small>event.code</small>
       </div>
     </div>
   );
