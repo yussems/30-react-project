6fe3056900ea93c41b7d56514c6a36973211f30f
yussems <yussems@gmail.com>
2021-12-29 01:28:41 +0300

    added score will be fixed input

--- 30-project/quizzApp/quiz-app/src/App.js
+++ 30-project/quizzApp/quiz-app/src/App.js
@@ -1,21 +1,28 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import { data } from "./data";
 import Question from "./Question";
 
 function App() {
   const [number, setNumber] = useState(0);
-  const [score, setScore] = useState(0)
+  const [score, setScore] = useState(0);
 
-  
-
-  if(number > data.length -1) {
-    return <h3>sorular bitti</h3>
+  if (number > data.length - 1) {
+    return (
+      <div>
+        <h3>sorular bitti </h3>
+        <p>puan durumunuz {score}</p>
+      </div>
+    );
   }
 
   return (
     <div className="App">
-      <Question  setNumber={setNumber}  questions={data[number]} />
+      <Question
+        setScore={setScore}
+        setNumber={setNumber}
+        questions={data[number]}
+      />
     </div>
   );
 }
--- 30-project/quizzApp/quiz-app/src/Question.js
+++ 30-project/quizzApp/quiz-app/src/Question.js
@@ -1,23 +1,24 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-function Question({ questions,setNumber }) {
+function Question({ questions, setNumber, setScore }) {
   const { question, a, b, c, d, correct } = questions;
-  const [choose, setChoose] = useState({
-      a:'',
-      b:'',
-      c:'',
-      d:'',
-  })
-  const textRef = useRef(null)
+  const [change, setChange] = useState("");
+  const [checked, setChecked] = useState('');
+
+  const handleChange = (e) => {
+    setChange(e.target.id);
+    setChecked(e.target.checked)
+  };
   const handleClick = () => {
-      setNumber(item => item + 1)
-      console.log(textRef.current.id);
+    setNumber((item) => item + 1);
+    setChecked('')
+    if (correct === change) {
+      setScore((item) => item + 1);
+    }
+  };
 
-}
-
-    
-
+  console.log(checked);
   return (
     <div>
       <>
@@ -25,30 +26,54 @@ function Question({ questions,setNumber }) {
           <h2>{question}</h2>
           <ul>
             <li>
-              <input  ref={textRef} id="a" type="radio" name="answer" className="answer" />
+              <input
+                onChange={handleChange}
+                id="a"
+                type="radio"
+                name="answer"
+                className="answer"
+              />
               <label htmlFor="a">{a}</label>
             </li>
           </ul>
           <ul>
             <li>
-              <input ref={textRef} id="b" type="radio" name="answer" className="answer" />
-              <label htmlFor="b"> {b} </label>
+              <input
+                onChange={handleChange}
+                id="b"
+                type="radio"
+                name="answer"
+                className="answer"
+              />
+              <label htmlFor="b">{b}</label>
             </li>
           </ul>
           <ul>
             <li>
-              <input ref={textRef} id="c" type="radio" name="answer" className="answer" />
+              <input
+                onChange={handleChange}
+                id="c"
+                type="radio"
+                name="answer"
+                className="answer"
+              />
               <label htmlFor="c">{c}</label>
             </li>
           </ul>
           <ul>
             <li>
-              <input ref={textRef} id="d" type="radio" name="answer" className="answer" />
+              <input
+                onChange={handleChange}
+                id="d"
+                type="radio"
+                name="answer"
+                className="answer"
+              />
               <label htmlFor="d">{d}</label>
             </li>
           </ul>
         </div>
-        <button  onClick={handleClick}>Submit</button>
+        <button onClick={handleClick}>Submit</button>
       </>
     </div>
   );
